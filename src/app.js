18267f4c6e47525946679cc246fd48df5da4fda5
Hit Santoki <hitsantoki40@gmail.com>
2025-02-06 17:08:56 +0530

    add user and video model

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from "cookie-parser";
+export const app = express();
 
 app.use(cors({
     origin : process.env.CORS_ORIGIN,
@@ -11,5 +12,3 @@ app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended : true, limit : "16kb"}))
 app.use(express.static("public"));
 app.use(cookieParser());
-
-export const app = express();
