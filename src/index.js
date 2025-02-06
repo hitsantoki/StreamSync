7346d9d0aefd9aacba3f597939907931bb3d5327
Hit Santoki <hitsantoki40@gmail.com>
2025-02-06 15:51:55 +0530

    utils implementation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 // import 'dotenv/config'
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import { configDotenv } from 'dotenv';
 
+
 configDotenv()
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error:",error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Server is running at Port:${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDb connection failed:",err);
+})
 
 
 // import mongoose from "mongoose";
